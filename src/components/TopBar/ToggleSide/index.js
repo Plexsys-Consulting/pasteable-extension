b625135c0a02ef7e973419939e85734bb497917e
Plexsys-Consulting <plexsys.llc@gmail.com>
2022-01-11 22:24:42 -0600

    Fixed small issue in styling.

--- src/components/TopBar/ToggleSide/index.js
+++ src/components/TopBar/ToggleSide/index.js
@@ -4,16 +4,18 @@ import { FaExchangeAlt } from "react-icons/fa";
 export default () => {
     const [side, setSide] = useState('right');
     return (
-        <FaExchangeAlt className='absolute top-3 right-3 text-emerald-200 cursor-pointer' onClick={() => {
-            if(side === 'left'){
-                document.getElementById('pasteable-root').style.left = 'unset';
-                setSide('right');
-            }
-            
-            if(side === 'right'){
-                document.getElementById('pasteable-root').style.left = 0;
-                setSide('left'); 
-            }
-        }} />
+        <div className='absolute top-3 right-3 text-emerald-200 cursor-pointer' >
+            <FaExchangeAlt onClick={() => {
+                if (side === 'left') {
+                    document.getElementById('pasteable-root').style.left = 'unset';
+                    setSide('right');
+                }
+
+                if (side === 'right') {
+                    document.getElementById('pasteable-root').style.left = 0;
+                    setSide('left');
+                }
+            }} />
+        </div>
     )
 }
