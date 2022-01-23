8e017ee0660062597ddcd304be0b9d2aba5bd223
Plexsys-Consulting <plexsys.llc@gmail.com>
2022-01-22 18:24:13 -0600

    Made iconList cleaner/less repetitive.

--- src/modules/Header/OptionIcons/index.js
+++ src/modules/Header/OptionIcons/index.js
@@ -1,38 +1,24 @@
 import { FaPlus, FaLevelUpAlt, FaLevelDownAlt, FaEdit } from 'react-icons/fa';
 import Icon from './Icon';
-import { engageElementSelection } from 'modules/common/functions';
 import { useViewContext } from 'modules/common/contexts/ViewContext';
-
-const selectEls = async () => {
-    try {
-        const selected = await engageElementSelection();
-        console.dir(selected)
-    } catch (error) {
-        console.error(error);
-    }
-}
 
 const iconList = [
     {
-        onClick: setShowView => { setShowView("create"); selectEls() },
-        activeView: "create",
+        linkedView: "create",
         iconComponent: <FaPlus />,
         text: "create copy/paste map"
     },
     {
-        onClick: setShowView => setShowView("run-copy"),
-        activeView: "run-copy",
+        linkedView: "run-copy",
         iconComponent: <FaLevelUpAlt />,
         text: "run copy"
     },
     {
-        onClick: setShowView => setShowView("run-paste"),
-        activeView: "run-paste",
+        linkedView: "run-paste",
         iconComponent: <FaLevelDownAlt />,
         text: "run paste"
     }, {
-        onClick: setShowView => setShowView("edit"),
-        activeView: "edit",
+        linkedView: "edit",
         iconComponent: <FaEdit />,
         text: "edit copy/paste maps"
     },
@@ -42,9 +28,10 @@ const OptionIcons = () => {
     const [showView, setShowView] = useViewContext();
     return (
         <div className='flex flex-row justify-left'>
-            {iconList.map(({ onClick, activeView, iconComponent, text }) => <Icon
-                onClick={() => onClick(setShowView)}
-                className={showView === activeView ? "bg-emerald-200 text-emerald-900 rounded-b-lg" : ""}
+            {iconList.map(({ linkedView, iconComponent, text }) => <Icon
+                key={linkedView}
+                onClick={() => setShowView(linkedView)}
+                className={showView === linkedView ? "bg-emerald-200 text-emerald-900 rounded-b-lg" : ""}
                 icon={iconComponent}
                 text={text}
             />)}
