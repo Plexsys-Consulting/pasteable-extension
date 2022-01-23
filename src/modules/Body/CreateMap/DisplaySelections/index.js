aa2343d16002515d10b0aeaf964baf3a48ba1a7c
Plexsys-Consulting <plexsys.llc@gmail.com>
2022-01-22 18:27:07 -0600

    Extracted subcomponents into separate components. Added component to display selected items.

---
+++ src/modules/Body/CreateMap/DisplaySelections/index.js
@@ -0,0 +1,7 @@
+const DisplaySelections = ({children}) => (
+    <div className="overflow-y-auto max-h-[70vh] my-3">
+        {children}
+    </div>
+)
+
+export default DisplaySelections;
--- src/modules/Body/CreateMap/index.js
+++ src/modules/Body/CreateMap/index.js
@@ -1,23 +1,40 @@
 import { useState } from 'react';
+import { FaSave } from 'react-icons/fa';
+import { MapContext } from 'modules/common/contexts/MapContext';
+import Select from 'modules/common/components/Select';
+import Button from 'modules/common/components/Button';
+import DisplaySelections from './DisplaySelections';
+import { selectElements } from 'modules/common/functions';
 
 const CreateMap = () => {
-    const [selected, setSelected] = useState("second");
+    const [selected, setSelected] = useState("copy");
     return (
-        <div className="p-2">
-            <select
-                value={selected}
-                onChange={e=> setSelected(e.target.value)}
-                className={`
-                    w-full p-2 rounded-md shadow-sm 
-                    shadow-emerald-600 bg-emerald-200 text-emerald-800
-                    active:outline-none focus:outline-none cursor-pointer
-                `}
-            >
-                <option value="first">First copy</option>
-                <option value="second">Second copy</option>
-                <option value="third">Third copy</option>
-            </select>
-        </div>
+        <MapContext>
+            <div className="p-2">
+                <Select
+                    value={selected}
+                    onChange={e => setSelected(e.target.value)}
+                    options={[
+                        {
+                            value: "copy",
+                            text: "create copy map"
+                        },
+                        {
+                            value: "paste",
+                            text: "create paste map"
+                        }
+                    ]}
+                />
+                <DisplaySelections>
+                    click elements in the page to add them to the {`${selected}`} map
+                </DisplaySelections>
+                <Button
+                    className="sticky bottom-3 w-full"
+                    icon={<FaSave className="inline mx-1 w-4" />}
+                    text={`save ${selected} map`}
+                />
+            </div>
+        </MapContext>
     )
 };
 
