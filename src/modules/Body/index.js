61d3ecba652644f602fd49ccea2d814c89ba9559
Plexsys-Consulting <plexsys.llc@gmail.com>
2022-02-19 15:10:32 -0600

    Wrapped SidebarViews with MapContext provider.

--- src/modules/Body/index.js
+++ src/modules/Body/index.js
@@ -1,18 +1,28 @@
 import { useViewContext } from "modules/common/contexts/ViewContext";
+import { MapContext } from "modules/common/contexts/MapContext";
 import CreateMap from "./CreateMap";
 import EditMaps from "./EditMaps";
 import RunCopy from "./RunCopy";
 import RunPaste from "./RunPaste";
 
-const SidebarViews = () => {
-    const [ showView ] = useViewContext();
-    switch(showView){
+const SwitchView = () => {
+    const [showView] = useViewContext();
+    
+    switch (showView) {
         case "create": return <CreateMap />;
-        case "run-copy": return <RunCopy />; 
+        case "run-copy": return <RunCopy />;
         case "run-paste": return <RunPaste />;
         case "edit": return <EditMaps />;
         default: return <CreateMap />;
     }
+}
+
+const SidebarViews = () => {
+    return (
+        <MapContext>
+            <SwitchView />
+        </MapContext>
+    )
 };
 
 export default SidebarViews;
