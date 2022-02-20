88325f01109e4931f446ac9feb0a93a4a0ef44da
Plexsys-Consulting <plexsys.llc@gmail.com>
2022-02-19 21:08:25 -0600

    Added remove icon for selectedEls and elHighlighting on hover.

--- src/modules/Body/CreateMap/DisplaySelections/index.js
+++ src/modules/Body/CreateMap/DisplaySelections/index.js
@@ -1,10 +1,27 @@
+import { useRef } from 'react';
 import TextInput from 'modules/common/components/TextInput';
+import Icon from 'modules/common/components/Icon';
+import { FaTrash } from 'react-icons/fa';
+import { useEffect } from 'react/cjs/react.development';
 
-const Element = ({id, path, name, text}) => (
-    <div className="flex flex-row" onMouseUp={() => document.getElementById(`${id}`).addClass('highlighted')}>
-        <TextInput value={name} />
-    </div>
-)
+const Element = ({id, path, name, text}) => {
+    const displayed = useRef(null);
+    useEffect(() => {
+        displayed.current.addEventListener('mouseover', () => document.getElementById(`${id}`).classList.add('highlighted'));
+        displayed.current.addEventListener('mouseleave', () => document.getElementById(`${id}`).classList.remove('highlighted'));
+    }, [displayed])
+
+    return (
+        <div className="flex flex-row cursor-pointer" ref={displayed}>
+            <TextInput value={name} />
+            <Icon 
+                icon={<FaTrash />}
+                className="hover:text-red-600 hover:cursor-pointer active:text-red-400 active:scale-[.97]"
+                tooltip="remove element"
+            />
+        </div>
+    )
+}
 
 const DisplaySelections = ({elements, copyPaste}) => (
     <div className="overflow-y-auto max-h-fit my-3">
